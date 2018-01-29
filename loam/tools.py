f5bd00c47d1d7cf08137a1eb62b809dadc8b4850
Adrien Morison <adrien.morison@gmail.com>
2018-01-29 20:00:43 +0100

    Improve tools API docs

--- loam/tools.py
+++ loam/tools.py
@@ -1,3 +1,5 @@
+"""Various helper functions and classes."""
+
 from collections import namedtuple
 import argparse
 
@@ -12,19 +14,40 @@ Subcmd = namedtuple('Subcmd', ['extra_parsers', 'defaults', 'help'])
 
 class Switch(argparse.Action):
 
-    """argparse Action to store True/False to a +/-arg"""
+    """Inherited from argparse.Action, store True/False to a +/-arg.
+
+    The :func:`switch_opt` function allows you to easily create a
+    :class:`ConfOpt` using this action.
+    """
 
     def __call__(self, parser, namespace, values, option_string=None):
-        """set args attribute with True/False"""
+        """Set args attribute with True/False"""
         setattr(namespace, self.dest, bool('-+'.index(option_string[0])))
 
 
 def bare_opt(default):
-    """Define a ConfOpt with only a default value."""
+    """Define a ConfOpt with only a default value.
+
+    Args:
+        default: the default value of the configuration option.
+
+    Returns:
+        :class:`ConfOpt`: a configuration option with the default value.
+    """
     return ConfOpt(default, False, None, {}, False, '')
 
 
 def switch_opt(default, shortname, help_msg):
-    """Define a ConfOpt with the Switch action."""
+    """Define a ConfOpt with the Switch action.
+
+    Args:
+        default (bool): the default value of the swith option.
+        shortname (str): short name of the option, no shortname will be used if
+            it is set to None.
+        help_msg (str): short description of the option.
+
+    Returns:
+        :class:`ConfOpt`: a configuration option with the given properties.
+    """
     return ConfOpt(
         bool(default), True, shortname, dict(action=Switch), True, help_msg)
