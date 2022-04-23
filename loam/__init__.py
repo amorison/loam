421839dc7f30d92d484e63cdaafe95fd2d473f8d
Adrien Morison <adrien.morison@gmail.com>
2022-04-23 15:02:53 +0100

    Remove setuptools_scm runtime dependency

--- loam/__init__.py
+++ loam/__init__.py
@@ -1,11 +1,6 @@
 """Easy config management setup for your Python project."""
 
-from setuptools_scm import get_version
-from pkg_resources import get_distribution, DistributionNotFound
-
 try:
-    __version__ = get_version(root='..', relative_to=__file__)
-except LookupError:
-    __version__ = get_distribution('loam').version
-except (DistributionNotFound, ValueError):
+    from ._version import version as __version__
+except ImportError:
     __version__ = 'unknown'
