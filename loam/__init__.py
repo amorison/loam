8d0753ecfbce8019e1c7c639d19dd9740f7bceba
Adrien Morison <adrien.morison@gmail.com>
2024-08-26 18:52:43 +0100

    version: no longer rely on setuptools_scm

--- loam/__init__.py
+++ loam/__init__.py
@@ -1,6 +1,5 @@
 """Easy config management setup for your Python project."""
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
+from importlib.metadata import version
+
+__version__ = version("loam")
